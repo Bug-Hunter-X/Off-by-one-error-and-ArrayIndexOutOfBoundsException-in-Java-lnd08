22fa8e9b0cc8a55b4b1902019ae3143e19c3faba
BugHunter-X <askzenai@gmail.com>
2024-12-09 11:06:05 +0100

    Add bugSolution.java file

---
+++ bugSolution.java
@@ -0,0 +1,10 @@
+public class MyClass {
+    public static void main(String[] args) {
+        int[] arr = new int[5];
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = i * 2; 
+        }
+        //Accessing a valid index
+        System.out.println(arr[4]);
+    }
+}
